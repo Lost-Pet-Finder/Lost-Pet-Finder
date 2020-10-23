375c8e6ff757dcad38c8eb1ff633f95fa861a205
nathan-n-poon <nathannpoon@gmail.com>
2020-10-23 11:55:49 -0600

    Update petsController.js

--- backend/src/controllers/petsController.js
+++ backend/src/controllers/petsController.js
@@ -32,5 +32,26 @@ async function postLostPets(req, res) {
         then call tags = awsFunctions.getAWSTags(data)
         and then leave one line thats like *store in SQL HERE*
      */
+    var filename = "";
+    var location = [0,0];
+    var date = "";
+    var userid = 0;
+    var tags = [];
+    var bucketName = "";
 
+    filename = req.body.filename;
+    location  = req.body.location;
+    date = req.body.date;
+    userid = req.body.userid;
+
+    var data = {
+            "bucketName": req.body.bucketName,
+            "fileName": req.body.filename
+    };
+
+    console.log(data);
+    // awsFunctions.getAWSTags(data).then((value) => tags = value);
+
+    //*Store in SQL here*
+    res.send(200);
 }
