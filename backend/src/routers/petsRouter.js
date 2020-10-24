32666a9fe490c339203de0a54fdd7cf6247e2c0c
nathanPoon <nathannpoon@gmail.com>
2020-10-23 19:55:32 -0600

    changed search functions to get requests

--- backend/src/routers/petsRouter.js
+++ backend/src/routers/petsRouter.js
@@ -9,9 +9,9 @@ router.get('/', (req, res) => {
     res.status(200).send('This is the pets endpoint');
 });
 
-router.post('/searchLostPets', petsController.searchLostPets);
+router.get('/searchLostPets', petsController.searchLostPets);
 router.post('/postLostPets', petsController.postLostPets);
-router.post('/searchFoundPets', petsController.searchFoundPets);
+router.get('/searchFoundPets', petsController.searchFoundPets);
 router.post('/postFoundPets', petsController.postFoundPets);
 
 module.exports = router;
