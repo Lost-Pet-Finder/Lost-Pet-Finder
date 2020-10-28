4185f3a77180fa67b65c0b023cf4302bf2c33f69
nathanPoon <nathannpoon@gmail.com>
2020-10-28 01:00:23 -0600

    ERROR: Cant transform cropped image to buffer ?????

--- backend/src/controllers/awsController.js
+++ backend/src/controllers/awsController.js
@@ -1,6 +1,29 @@
 // Imports
 // const sqlPool = require('../sql/connection');
 const rekognition = require('../aws/rekognitionClient');
+const colorThief = require('color-thief');
+const sizeOf = require('buffer-image-size');
+// const Jimp = require('jimp');
+// var Clipper = require('image-clipper');
+const gm = require('gm')
+// var fs = require('fs')
+
+var AWS = require('aws-sdk');
+const ColorThief = require('color-thief');
+const { head } = require('../routers/awsRouter');
+
+const config = new AWS.Config({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+  });
+
+
 
 async function sendRekognitionRequest(req, res) {
     const bucketName = req.body.bucketName;
@@ -13,10 +36,13 @@ async function sendRekognitionRequest(req, res) {
                 Name: fileName
             }
         },
-        MaxLabels: 10
+        MaxLabels: 100
     }
 
     try {
+        const image = await s3.getObject({Bucket: bucketName,Key: fileName}).promise();
+        // image = fs.createReadStream(image.Body)
+
         const response = await rekognition.detectLabels(params).promise();
 
         var returnedLabels = [];
@@ -24,6 +50,52 @@ async function sendRekognitionRequest(req, res) {
         response.Labels.forEach(label => {
             returnedLabels.push(label);
         });
+
+        var box;
+        response.Labels.forEach(label => {
+            console.log("Instances:")
+            label.Instances.forEach(instance => {
+                box = instance.BoundingBox
+            })
+        })
+
+        var totalDimensions = sizeOf(image.Body);
+        totalWidth = totalDimensions.width;
+        totalHeight = totalDimensions.height;
+
+        topLeft = [box.Left * totalWidth, box.Top * totalHeight];
+
+        boxWidth = box.Width * totalWidth;
+        boxHeight = box.Height * totalHeight;
+
+        img = gm(image.Body).crop(boxWidth, boxHeight, topLeft[0], topLeft[1]).toBuffer(function(err, buffer) {
+            if(err)
+            console.log("oh no!")
+        })
+
+        
+        console.log(boxWidth , boxHeight);
+        var dim = sizeOf(img)
+        x = dim.width
+        y = dim.height
+        console.log(x, y);
+
+        // console.log(returnedLabels)
+
+
+
+        var color =  new ColorThief();
+
+        // mode = color.getColor(image.Body);
+
+        // pixels = color.getPalette(image.Body, 8);
+        // mode = (pixels.sort((a,b) =>
+        //   pixels.filter(v => v===a).length
+        // - pixels.filter(v => v===b).length).pop());
+
+        // console.log(mode);
+
+
 
         res.status(200).json({
             labels: returnedLabels
