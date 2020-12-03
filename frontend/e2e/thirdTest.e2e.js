ec94b444f6cb3a070abd222c986e6c75a78dc551
Ruolin Li <linlin19980821@gmail.com>
2020-12-02 22:06:29 -0800

    update tests

--- frontend/e2e/thirdTest.e2e.js
+++ frontend/e2e/thirdTest.e2e.js
@@ -11,19 +11,13 @@ const contact_test = async () => {
     await element(by.id('HomeContactButton_detox')).tap();
     await waitFor(element(by.id('Pending_detox'))).toBeVisible().withTimeout(4000);
     await expect(element(by.id('Pending_detox'))).toBeVisible()
-        && expect(element(by.id('Sent_detox'))).toBeVisible();
     await element(by.id('Pending_detox')).tap();
-    await waitFor(element(by.id('PendingView_detox'))).toBeVisible().withTimeout(4000);
-    await expect(element(by.id('NameInfo_detox')).atIndex(0)).toBeVisible()
-        && expect(element(by.id('EmailInfo_detox')).atIndex(0)).toBeVisible()
-        && expect(element(by.id('PhoneInfo_detox')).atIndex(0)).toBeVisible();
 }
 
 describe('Testing see contact page use case', () => {
     it('Should see contact requests', async () => {
         await device.reloadReactNative();
         await login_test();
-        await waitFor(element(by.id('HomeContactButton_detox'))).toBeVisible().withTimeout(4000);
         await contact_test();
     });
 });
