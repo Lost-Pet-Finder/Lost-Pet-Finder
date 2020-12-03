e2817dc476977179bf01213e2f7e39a4f15e2cbf
Hung Nguyen <hunglnl01@gmail.com>
2020-12-02 20:20:31 -0800

    codacy fix

--- backend/src/sql/__mocks__/connection.js
+++ backend/src/sql/__mocks__/connection.js
@@ -8,57 +8,57 @@ const {
 } = require('../../tests/testVariables');
 
 const query = (string, userid) => {
-	if (string == 'CALL get_all_lost_pets()') {
+	if (string === 'CALL get_all_lost_pets()') {
 		return allReportsRowsLost;
-	} else if (string == 'CALL get_all_found_pets()') {
+	} else if (string === 'CALL get_all_found_pets()') {
 		return allReportsRowsFound;
 	} else if (
-		string == 'CALL create_lost_pet_report(?, ?, POINT(?,?), ?, ?, ?)' &&
+		string === 'CALL create_lost_pet_report(?, ?, POINT(?,?), ?, ?, ?)' &&
 		// invalid month
-		userid[4] != '202099 0303'
+		userid[4] !== '202099 0303'
 	) {
 		return expectedPostLostPets;
 	} else if (
-		string == 'CALL create_lost_pet_report(?, ?, POINT(?,?), ?, ?, ?)' &&
+		string === 'CALL create_lost_pet_report(?, ?, POINT(?,?), ?, ?, ?)' &&
 		// invalid month
-		userid[4] == '202099 0303'
+		userid[4] === '202099 0303'
 	) {
 		return null;
 	} else if (
-		string == 'CALL create_found_pet_report(?, ?, POINT(?,?), ?, ?, ?)' &&
+		string === 'CALL create_found_pet_report(?, ?, POINT(?,?), ?, ?, ?)' &&
 		// invalid month
-		userid[4] != '202099 0303'
+		userid[4] !== '202099 0303'
 	) {
 		return expectedPostFoundPets;
 	} else if (
-		string == 'CALL create_found_pet_report(?, ?, POINT(?,?), ?, ?, ?)' &&
+		string === 'CALL create_found_pet_report(?, ?, POINT(?,?), ?, ?, ?)' &&
 		// invalid month
-		userid[4] == '202099 0303'
+		userid[4] === '202099 0303'
 	) {
 		return null;
-	} else if (string == 'CALL get_users_found_reports(?)' && userid != 99999) {
+	} else if (string === 'CALL get_users_found_reports(?)' && userid !== 99999) {
 		return myReportRowsLost;
-	} else if (string == 'CALL get_users_found_reports(?)' && userid == 99999) {
+	} else if (string === 'CALL get_users_found_reports(?)' && userid === 99999) {
 		return [1];
-	} else if (string == 'CALL get_users_lost_reports(?)' && userid != 99999) {
+	} else if (string === 'CALL get_users_lost_reports(?)' && userid !== 99999) {
 		return myReportRowsFound;
-	} else if (string == 'CALL get_users_lost_reports(?)' && userid == 99999) {
+	} else if (string === 'CALL get_users_lost_reports(?)' && userid === 99999) {
 		return [1];
-	} else if (string == 'CALL get_user_contact_info(?)' && userid != 99999) {
+	} else if (string === 'CALL get_user_contact_info(?)' && userid !== 99999) {
 		const data = [[0], [1]];
 		return data;
 	} else if (
-		string == 'CALL create_new_app_user(?, ?, ?, ?)' &&
-		userid[0] != 99999
+		string === 'CALL create_new_app_user(?, ?, ?, ?)' &&
+		userid[0] !== 99999
 	) {
 		const data = [[0], [1]];
 		return data;
 	} else if (
-		string == 'CALL create_new_app_user(?, ?, ?, ?)' &&
-		userid[0] == 99999
+		string === 'CALL create_new_app_user(?, ?, ?, ?)' &&
+		userid[0] === 99999
 	) {
 		return null;
-	} else if (string == 'CALL get_user_id_number(?)' && userid != 99999) {
+	} else if (string === 'CALL get_user_id_number(?)' && userid !== 99999) {
 		const data = [
 			[{ user_id: 321 }],
 			{
@@ -73,23 +73,29 @@ const query = (string, userid) => {
 			},
 		];
 		return data;
-	} else if (string == 'CALL get_my_pending_requests(?)' && userid != 99999) {
+	} else if (string === 'CALL get_my_pending_requests(?)' && userid !== 99999) {
 		const data = [[0], [1]];
 		return data;
-	} else if (string == 'CALL get_my_sent_requests(?)' && userid != 99999) {
+	} else if (string === 'CALL get_my_sent_requests(?)' && userid !== 99999) {
 		const data = [[0], [1]];
 		return data;
-	} else if (string == 'CALL get_user_device_token(?)' && userid != 99999) {
+	} else if (string === 'CALL get_user_device_token(?)' && userid !== 99999) {
 		const data = [[0], [1]];
 		return data;
-	} else if (string == 'CALL upload_device_token(?,?)' && userid[0] != 99999) {
+	} else if (
+		string === 'CALL upload_device_token(?,?)' &&
+		userid[0] !== 99999
+	) {
 		const data = [[0], [1]];
 		return data;
-	} else if (string == 'CALL upload_device_token(?,?)' && userid[0] == 99999) {
+	} else if (
+		string === 'CALL upload_device_token(?,?)' &&
+		userid[0] === 99999
+	) {
 		return null;
-	} else if (string == 'CALL send_contact_request(?,?)') {
+	} else if (string === 'CALL send_contact_request(?,?)') {
 		return 1;
-	} else if ((string = 'CALL get_user_contact_info(?)' && userid != 99999)) {
+	} else if (string === 'CALL get_user_contact_info(?)' && userid !== 99999) {
 		return [
 			[
 				{
@@ -114,7 +120,10 @@ const query = (string, userid) => {
 				changedRows: 0,
 			},
 		];
-	} else if ('CALL get_user_device_token(?)' && userid[0] != 99999) {
+	} else if (
+		string === 'CALL get_user_device_token(?)' &&
+		userid[0] !== 99999
+	) {
 		return [[0], [1]];
 	}
 };
